5bb370dd937d4bde66972ba76b73c7c5dae12e46
Mathilde Morell <morell.mathilde74@gmail.com>
2025-05-25 18:20:35 +0200

    add the price and statut in the user db

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -11,8 +11,8 @@ class UserController
         return $user->all();
     }
 
-    public function addUser(string $last_name, string $first_name, string $adress, string $dish): void {
-        $user = new User($last_name, $first_name, $adress, $dish);
+    public function addUser(string $last_name, string $first_name, string $adress, string $dish, float $price, string $statut): void {
+        $user = new User($last_name, $first_name, $adress, $dish, $price, $statut);
         $user->save();
     }
 
@@ -26,8 +26,13 @@ class UserController
         if (empty($last_name) || empty($first_name) || empty($adress) || empty($dish)) {
             throw new Exception("Tous les champs sont obligatoires.");
         }
-    
-        $this->addUser($last_name, $first_name, $adress, $dish);
+
+        $price = mt_rand(1000, 10000) / 100;
+        $statut = 'Commande prise en compte';
+        
+
+
+        $this->addUser($last_name, $first_name, $adress, $dish, $price, $statut);
     }
     
 }
--- app/models/user.php
+++ app/models/user.php
@@ -12,23 +12,30 @@ class User extends Bdd {
     private string $first_name;
     private string $adress;
     private string $dish;
+    private float $price;
+    private string $statut;
 
-    public function __construct(string $last_name = '', string $first_name = '', string $adress = '', string $dish = '') {
+
+    public function __construct(string $last_name = '', string $first_name = '', string $adress = '', string $dish = '', float $price =  0.0, string $statut = '') {
         parent::__construct();
         $this->last_name = $last_name;
         $this->first_name = $first_name;
         $this->adress = $adress;
         $this->dish = $dish;
+        $this->price = $price;
+        $this->statut = $statut;
     }
 
     public function save(): bool {
-        $sql = "INSERT INTO users (last_name, first_name, adress, dish) VALUES (:last_name, :first_name, :adress, :dish)";
+        $sql = "INSERT INTO users (last_name, first_name, adress, dish, price, statut) VALUES (:last_name, :first_name, :adress, :dish, :price, :statut)";
         $stmt = $this->co->prepare($sql);
         return $stmt->execute([
             ':last_name' => $this->last_name,
             ':first_name' => $this->first_name,
             ':adress' => $this->adress,
-            ':dish' => $this->dish
+            ':dish' => $this->dish,
+            ':price' => $this->price,
+            ':statut' => $this->statut
         ]);
     }
 
